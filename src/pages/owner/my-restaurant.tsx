9c6904e8f783ce2726a1da65fe84642166eb4f6e
kmsk99 <momoqq@naver.com>
2021-12-29 11:02:22 +0900

    #22 MAKING AN ORDER

--- src/pages/owner/my-restaurant.tsx
+++ src/pages/owner/my-restaurant.tsx
@@ -1,9 +1,29 @@
-import { gql, useQuery } from '@apollo/client';
+import { gql, useMutation, useQuery } from '@apollo/client';
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import { Link, useParams } from 'react-router-dom';
 import { Dish } from '../../components/dish';
-import { DISH_FRAGMENT, RESTAURANT_FRAGMENT } from '../../fragments';
+import {
+    VictoryAxis,
+    VictoryBar,
+    VictoryChart,
+    VictoryLabel,
+    VictoryLine,
+    VictoryPie,
+    VictoryTheme,
+    VictoryTooltip,
+    VictoryVoronoiContainer,
+} from 'victory';
+import {
+    DISH_FRAGMENT,
+    ORDERS_FRAGMENT,
+    RESTAURANT_FRAGMENT,
+} from '../../fragments';
+import { useMe } from '../../hooks/useMe';
+import {
+    createPayment,
+    createPaymentVariables,
+} from '../../__generated__/createPayment';
 import {
     myRestaurant,
     myRestaurantVariables,
@@ -19,11 +39,24 @@ export const MY_RESTAURANT_QUERY = gql`
                 menu {
                     ...DishParts
                 }
+                orders {
+                    ...OrderParts
+                }
             }
         }
     }
     ${RESTAURANT_FRAGMENT}
     ${DISH_FRAGMENT}
+    ${ORDERS_FRAGMENT}
+`;
+
+const CREATE_PAYMENT_MUTATION = gql`
+    mutation createPayment($input: CreatePaymentInput!) {
+        createPayment(input: $input) {
+            ok
+            error
+        }
+    }
 `;
 
 export const MyRestaurant = () => {
@@ -39,6 +72,39 @@ export const MyRestaurant = () => {
         }
     );
     console.log(data);
+    const onCompleted = (data: createPayment) => {
+        if (data.createPayment.ok) {
+            alert('Your restaurant is being promoted!');
+        }
+    };
+    const [createPaymentMutation, { loading }] = useMutation<
+        createPayment,
+        createPaymentVariables
+    >(CREATE_PAYMENT_MUTATION, {
+        onCompleted,
+    });
+    const { data: userData } = useMe();
+    const triggerPaddle = () => {
+        if (userData?.me.email) {
+            // @ts-ignore
+            window.Paddle.Setup({ vendor: 31465 });
+            // @ts-ignore
+            window.Paddle.Checkout.open({
+                product: 638793,
+                email: userData.me.email,
+                successCallback: (data: any) => {
+                    createPaymentMutation({
+                        variables: {
+                            input: {
+                                transactionId: data.checkout.id,
+                                restaurantId: +id!,
+                            },
+                        },
+                    });
+                },
+            });
+        }
+    };
     return (
         <div>
             <Helmet>
@@ -46,7 +112,9 @@ export const MyRestaurant = () => {
                     {data?.myRestaurant.restaurant?.name || 'Loading...'} |
                     Nuber Eats
                 </title>
+                <script src="https://cdn.paddle.com/paddle/paddle.js"></script>
             </Helmet>
+            <div className="checkout-container"></div>
             <div
                 className="bg-gray-700 bg-center bg-cover py-28"
                 style={{
@@ -63,23 +131,77 @@ export const MyRestaurant = () => {
                 >
                     Add Dish &rarr;
                 </Link>
-                <Link to={``} className="px-10 py-3 text-white bg-lime-700">
+                <span
+                    onClick={triggerPaddle}
+                    className="px-10 py-3 text-white cursor-pointer bg-lime-700"
+                >
                     Buy Promotion &rarr;
-                </Link>
+                </span>
                 <div className="mt-10">
                     {data?.myRestaurant.restaurant?.menu.length === 0 ? (
                         <h4 className="mb-5 text-xl">Please upload a dish!</h4>
                     ) : (
                         <div className="grid mt-16 md:grid-cols-3 gap-x-5 gap-y-10">
-                            {data?.myRestaurant.restaurant?.menu.map((dish) => (
-                                <Dish
-                                    name={dish.name}
-                                    description={dish.description}
-                                    price={dish.price}
-                                />
-                            ))}
+                            {data?.myRestaurant.restaurant?.menu.map(
+                                (dish, index) => (
+                                    <Dish
+                                        key={index}
+                                        name={dish.name}
+                                        description={dish.description}
+                                        price={dish.price}
+                                    />
+                                )
+                            )}
                         </div>
                     )}
+                </div>
+                <div className="mt-20 mb-10">
+                    <h4 className="text-2xl font-medium text-center">Sales</h4>
+                    <div className="mt-10 ">
+                        <VictoryChart
+                            height={500}
+                            theme={VictoryTheme.material}
+                            width={window.innerWidth}
+                            domainPadding={50}
+                            containerComponent={<VictoryVoronoiContainer />}
+                        >
+                            <VictoryLine
+                                labels={({ datum }) => `$${datum.y}`}
+                                labelComponent={
+                                    <VictoryTooltip
+                                        style={{ fontSize: 18 } as any}
+                                        renderInPortal
+                                        dy={-20}
+                                    />
+                                }
+                                data={data?.myRestaurant.restaurant?.orders.map(
+                                    (order) => ({
+                                        x: order.createdAt,
+                                        y: order.total,
+                                    })
+                                )}
+                                interpolation="natural"
+                                style={{
+                                    data: {
+                                        strokeWidth: 5,
+                                    },
+                                }}
+                            />
+                            <VictoryAxis
+                                tickLabelComponent={
+                                    <VictoryLabel renderInPortal />
+                                }
+                                style={{
+                                    tickLabels: {
+                                        fontSize: 20,
+                                    } as any,
+                                }}
+                                tickFormat={(tick) =>
+                                    new Date(tick).toLocaleDateString('ko')
+                                }
+                            />
+                        </VictoryChart>
+                    </div>
                 </div>
             </div>
         </div>
